10773e6057dbabfe8423730139a1f4d9a5cbb761
Jason Matthews <jmatty1983@gmail.com>
2015-01-30 17:38:23 -0600

    Update knight_travails.rb
    some commenting

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,6 +1,10 @@
+#I needed a way to store the path being traversed when using the knight_moves method. The queue gets populated with
+#Location structures and updates the path with each iteration. When a solution is found the path member has an array
+#of points from start..finish.
 Location = Struct.new(:location, :path)
 
 class Knight
+    #An array of valid move operations a knight can make
     @@moves = [[ 1,  2], [ 2,  1], [ 1, -2], [ 2, -1],
                [-1,  2], [-2,  1], [-1, -2], [-2, -1]]
                
@@ -8,6 +12,7 @@ class Knight
         @board_size = board_size
     end
     
+    #uses BFS to find the shorteste path for a knight to move from one location to another
     def knight_moves(loc, dest)
         return nil unless valid_loc?(dest) || valid_loc?(loc)
         return loc if loc == dest
@@ -28,25 +33,25 @@ class Knight
         nil
     end
     
-    def knight_moves_helper(loc, dest, path = [])
-        
-    end
-    
+    #returns an array of valid moves from a given location
     def get_moves(loc)
         result = []
         @@moves.each { |p| result << add_move(loc, p) if valid_loc?(add_move(loc, p)) }
         result
     end
     
+    #add the x-coords and y-coords of 2 points and return a point
     def add_move(loc, move)
        [loc[0] + move[0], loc[1] + move[1]]
     end
     
+    #check if the location specified is on the board
     def valid_loc?(point)
        return true if point[0].between?(0, @board_size - 1) && point[1].between?(0, @board_size - 1)
        false
     end
 end
 
+#test
 knight = Knight.new
 knight.knight_moves([3,3],[4,3])
